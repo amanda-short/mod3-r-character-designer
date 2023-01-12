b0fe73592ce84391d0ee2a1bb6505e5fcda72342
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-11 17:26:07 -0800

    all state added for something to follow while I build the functions. Changed location of state from Main.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './components/Header/Header.js';
@@ -8,13 +8,35 @@ import Select from './components/Select/Select.js';
 import Phrase from './components/Phrase/Phrase.js';
 
 function App() {
+  const [head, setHead] = useState('');
+  const [middle, setMiddle] = useState('');
+  const [bottom, setBottom] = useState('');
+  const [phraseInput, setPhraseInput] = useState('');
+  const [phraseOutput, setPhraseOutput] = useState('');
+  const [headCount, setHeadCount] = useState('');
+  const [middleCount, setMiddleCount] = useState('');
+  const [bottomCount, setBottomCount] = useState('');
+
   return (
     <div className="App">
       <Header />
-      <Main />
-      <Character />
-      <Select />
-      <Phrase />
+      <Main {...{ head, setHead, middle, setMiddle, bottom, setBottom }} />
+      <Character {...{ head, setHead, middle, setMiddle, bottom, setBottom }} />
+      <Select {...{ head, middle, bottom }} />
+      <Phrase
+        {...{
+          phraseInput,
+          setPhraseInput,
+          phraseOutput,
+          setPhraseOutput,
+          headCount,
+          setHeadCount,
+          middleCount,
+          setMiddleCount,
+          bottomCount,
+          setBottomCount,
+        }}
+      />
     </div>
   );
 }
