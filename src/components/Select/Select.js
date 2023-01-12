a2ba970d512677e7a3011dc8c55334568289771e
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-11 17:44:06 -0800

    started building select drop downs and functions, not finished. End of day push

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import './Select.css';
 
-export default function Select() {
-  return <></>;
+export default function Select({ head, setHead, middle, setMiddle, bottom, setBottom }) {
+  const handleChange = (event) => {
+    setHead(event.target.value);
+    setHead((currentState) => [...currentState, event.target.value]);
+  };
+
+  return (
+    <div className="select">
+      <h1>Pick a head</h1>
+      <select value={head} onChange={handleChange}>
+        <option value="dog">Dog</option>
+        <option value="duck">Duck</option>
+        <option value="horse">Horse</option>
+        <option value="bird">Bird</option>
+      </select>
+    </div>
+  );
 }
